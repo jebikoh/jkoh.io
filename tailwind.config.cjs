750369eb16bdfa1a678ba9875558a4f1c5174335
jebi <aedara@uw.edu>
2023-09-01 03:45:03 -0400

    Update website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'text': 'hsl(189, 87%, 97%)',
-				'background': 'hsl(0, 0%, 0%)',
-				'primary': 'hsl(216, 82%, 27%)',
-				'secondary': 'hsl(246, 82%, 13%)',
-				'accent': 'hsl(246, 82%, 53%)',
-			}
-		},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                text: "hsl(189, 87%, 97%)",
+                background: "hsl(0, 0%, 0%)",
+                primary: "hsl(216, 82%, 27%)",
+                secondary: "hsl(246, 82%, 13%)",
+                accent: "hsl(246, 82%, 53%)",
+            },
+            fontFamily: {
+                mono: ["BB-Regular", ...defaultTheme.fontFamily.mono],
+            },
+        },
+    },
+    plugins: [],
+};
